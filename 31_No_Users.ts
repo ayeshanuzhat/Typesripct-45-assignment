b058ba2fc2326cbb38eb5300c2d52f8d07fdb32d
ayeshanuzhat <162364936+ayeshanuzhat@users.noreply.github.com>
2024-05-13 12:29:49 -0700

    Add files via upload

---
+++ 31_No_Users.ts
@@ -0,0 +1,10 @@
+
+
+let userNames: string[] = ['Admin','Naeem','Zeeshan','Kamran'];
+
+if(userNames.length === 0){
+    console.log('We need to find some users!');
+}else{
+    userNames = [];
+    console.log('All user Names have been removed. ' + userNames.length);
+}
