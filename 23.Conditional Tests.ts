fcc4852d5260b5f4d9d324d3aecf681982eb7d6b
ayeshanuzhat <162364936+ayeshanuzhat@users.noreply.github.com>
2024-05-13 12:14:17 -0700

    Add files via upload

---
+++ 23.Conditional Tests.ts
@@ -0,0 +1,47 @@
+
+
+
+let car : string = 'subaru' ;
+
+console.log("Is car === 'subaru' ? I predict True. ");
+console.log(car ==='subaru');
+
+
+
+console.log("Is car == 'subaru' ? I predict True. ");
+console.log(car =='subaru');
+
+
+console.log("Is car != 'Honda' ? I predict True. ");
+console.log(car !='Honda');
+
+
+console.log("Is car !== 'Ford' ? I predict True. ");
+console.log(car !=='Ford');
+
+
+console.log("Is car.Upper case == 'SUBARU' ? I predict True. ");
+console.log(car.toUpperCase() =='SUBARU');
+
+
+console.log("Is car == 'SUBARU' ? I predict False. ");
+console.log(car =='SUBARU');
+
+// test no 7
+console.log("Is car === 'SUBARU' ? I predict False. ");
+console.log(car === 'SUBARU');
+
+
+// test no 8
+console.log("Is car === 'Train' ? I predict False. ");
+console.log(car === 'Train');
+
+
+// test no 9
+console.log("Is car === 'Bus' ? I predict False. ");
+console.log(car === 'Bus');
+
+// test no 10
+console.log("Is car === 'Cycle' ? I predict False. ");
+console.log(car === 'Cycle');
+
