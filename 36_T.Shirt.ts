e1d342b6e7ea371199756ddb51012fa1a94c148d
ayeshanuzhat <162364936+ayeshanuzhat@users.noreply.github.com>
2024-05-13 12:40:44 -0700

    Add files via upload

---
+++ 36_T.Shirt.ts
@@ -0,0 +1,12 @@
+// Call the function.
+
+function make_shirt(size:string, text:string ){
+
+    console.log(`Creating a ${size} shirt with the message: ${text}`);
+}
+
+make_shirt('large','Hello world');
+
+make_shirt('medium','Hello worid');
+
+make_shirt('small','Hello world');
