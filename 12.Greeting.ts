d0ad40edc32baba2831c434090ece38e0c7f34d5
ayeshanuzhat <162364936+ayeshanuzhat@users.noreply.github.com>
2024-05-13 11:52:10 -0700

    Add files via upload

---
+++ 12.Greeting.ts
@@ -0,0 +1,13 @@
+
+
+let names :string[ ] = ['Ayesha','Asia','Muskan','Wania'] ;
+
+const message :string = 'Welcome to Typescript class: '
+
+for(let i=0; i<names.length; i++){
+
+    console.log(message + names[i]) ;
+
+}
+    
+
