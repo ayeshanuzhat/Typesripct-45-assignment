a4d22381c7ef47cabd1bb34b4cf2ed0bd9c4aa0e
ayeshanuzhat <162364936+ayeshanuzhat@users.noreply.github.com>
2024-05-13 12:22:27 -0700

    Add files via upload

---
+++ 27_Alien_Color_3.ts
@@ -0,0 +1,50 @@
+
+
+let alienColors: string = "green";
+
+// version 1 of the program
+if(alienColors === "green"){
+    console.log("player earned 5 points.");
+}
+else if(alienColors === "yellow"){
+    console.log("player earned 10 points.");
+}
+else if(alienColors === "red"){
+    console.log("player earned 15 points.");
+}
+else{
+    console.log("Please select right color")
+}
+
+// Version 2 of the program
+alienColors = "yellow";
+
+if(alienColors === "green"){
+    console.log("player earned 5 points.");
+}
+else if(alienColors === "yellow"){
+    console.log("player earned 10 points.");
+}
+else if(alienColors === "red"){
+    console.log("plater earned 15 points.");
+}
+else{
+    console.log("Please select right color")
+}
+
+
+// Version 3 of the program
+alienColors = "red";
+
+if(alienColors === "green"){
+    console.log("player earned 5 points.");
+}
+else if(alienColors === "yellow"){
+    console.log("player earned 10 points.");
+}
+else if(alienColors === "red"){
+    console.log("player earned 15 points.");
+}
+else{
+    console.log("Please select right color")
+}
