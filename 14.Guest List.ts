1c483e4f3f20d7442aa9c2ad1c3b90dec0664f63
ayeshanuzhat <162364936+ayeshanuzhat@users.noreply.github.com>
2024-05-13 11:55:53 -0700

    Add files via upload

---
+++ 14.Guest List.ts
@@ -0,0 +1,4 @@
+let Guest_List : string [] = ['Ayesha','Isha','Wania','Eman'];
+for(let i=0; i<Guest_List.length; i++){
+    console.log('Respected Madam' + Guest_List[i] + ',\nWe invited you on dinner tomorrow.\n\nThank You')
+}
