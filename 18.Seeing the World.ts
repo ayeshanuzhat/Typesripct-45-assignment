fa94397b5f3c0c35dae4c66b7c9a1299208021fd
ayeshanuzhat <162364936+ayeshanuzhat@users.noreply.github.com>
2024-05-13 12:04:47 -0700

    Add files via upload

---
+++ 18.Seeing the World.ts
@@ -0,0 +1,20 @@
+
+
+let places : string [] = ['Cape Town','Delhi','Faisalabad','Bankok','Lahore'];
+console.log('Orignal ' + places);
+
+console.log('Copy ' + [...places].sort());
+
+console.log('Orignal ' + places);
+
+console.log('Copy ' + [...places].sort().reverse());
+
+console.log('Orignal ' + places);
+
+console.log('Orignal ' + places.reverse());
+
+console.log('Orignal ' + places.reverse());
+
+console.log('Orignal ' + places.sort());
+
+console.log('Orignal ' + places.sort().reverse());
