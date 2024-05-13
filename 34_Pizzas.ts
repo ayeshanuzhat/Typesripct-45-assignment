4ec1caeee2f153c5a5c2beef30be1d19801a0dd3
ayeshanuzhat <162364936+ayeshanuzhat@users.noreply.github.com>
2024-05-13 12:36:59 -0700

    Add files via upload

---
+++ 34_Pizzas.ts
@@ -0,0 +1,10 @@
+
+
+let Pizzas: string[] = ['Pepperoni','Cuban','BBQ Chicken'];
+
+for(let Pizza of Pizzas){
+
+    console.log(`I like ${Pizza} pizza.`);
+}
+
+console.log(`I really love pizza!`);
