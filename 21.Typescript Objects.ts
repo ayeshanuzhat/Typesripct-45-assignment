d866186e36dc49bd3fd3d169fddbd209d837a058
ayeshanuzhat <162364936+ayeshanuzhat@users.noreply.github.com>
2024-05-13 12:10:03 -0700

    Add files via upload

---
+++ 21.Typescript Objects.ts
@@ -0,0 +1,39 @@
+
+
+
+let person_Name :string = 'Nuzhat';
+
+const parsonName_Array :string[] = ['Person','Car','Cold Drink'];
+
+
+interface person{
+    age : number,
+    name : string,
+    nationality : string,
+    Student : Boolean
+}
+
+let person = {
+
+    age : 40 ,
+    name :'Nuzhat' ,
+    nationality : 'Pakistan' ,
+    Student : true
+}
+
+console.log(person);
+
+
+interface car {
+    maker : string;
+    color : string;
+    automatic : Boolean
+}
+
+let car = {
+    maker : 'Toyota' ,
+    color : 'Black' ,
+    automatic : true
+}
+
+console.log(car);
