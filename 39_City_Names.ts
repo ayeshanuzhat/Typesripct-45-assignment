1e3a92b5b33de4bd5063b78ea2c26173b3982356
ayeshanuzhat <162364936+ayeshanuzhat@users.noreply.github.com>
2024-05-14 03:23:26 -0700

    Add files via upload

---
+++ 39_City_Names.ts
@@ -0,0 +1,13 @@
+
+
+function city_country(city: string, country: string){
+
+    return `${city},${country}`;
+}
+
+
+console.log (city_country('karachi','Pakistan'));
+
+console.log (city_country('Tokyo','Japan'));
+
+console.log (city_country('Paris','France'));
